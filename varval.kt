714329394cb7764d4a365376b2cd0a0c1ab0b3e8
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-08-10 20:19:10 -0600

    Create varval.kt
    Var and Val

---
+++ varval.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var cartTot = 0
+    cartTot = 20
+    println("Total: $cartTot")
+    //use val keyword when you expect the variable will
+    //not change. use var keyword when u expect the variable
+    //value to change
+    val trip1: Double = 2.30
+    println("Trip 1 value: $trip1")
+    val name = "Mar"
+    println("$name")
+    val lname = "Gali"
+    val cname = name + lname
+    println(cname)
+}
